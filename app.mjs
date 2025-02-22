733eff9b28b90b615bdd2b8eade1b5dff7b0534c
Luis Gonzalo Morelli <gonzy.morelli@gmail.com>
2025-02-22 14:36:46 -0300

    Server Normal

---
+++ app.mjs
@@ -0,0 +1,21 @@
+import express from 'express'
+
+const app = express();
+
+const PORT = 3000;
+
+app.get('/user/:id', (req, res)=>{
+    const userId = req.params.id
+    console.log(`ID del usuario recibido: ${userId}`);
+    res.send(`Perfil del usuario con ID: ${userId}`);
+});
+
+app.get('/profile', (req, res)=>{
+    const edad = req.query.edad;
+    console.log(`Edad recibida: ${edad}`);
+    res.send(`Edad del perfil: ${edad}`);
+});
+
+app.listen(PORT,() => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+})
